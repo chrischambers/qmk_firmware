7c7f5af47cbad257fafe6e37046235291f778abe
Chris Chambers <magma.chambers@gmail.com>
2016-03-26 17:25:18 +0000

    Fix plover modifications

--- keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
+++ keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
@@ -136,8 +136,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*-*/      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     /*-*/      KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_NO,
-    /*-*/      /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_NO,
-    /*-*/      KC_TRNS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_NO,
+    /*-*/      /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,
+    /*-*/      KC_TRNS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
     /*-*/      /*-*/    /*-*/    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     KC_TRNS,   KC_TRNS,
     KC_TRNS,
