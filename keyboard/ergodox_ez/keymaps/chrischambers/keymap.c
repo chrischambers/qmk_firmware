2bfc47f91b76381d7e8e674c0cc9b8e3f53fa7fa
Chris Chambers <magma.chambers@gmail.com>
2016-03-26 14:36:25 +0000

    Better arrow layer, more diagrams, fix colon to semi in base layer

--- keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
+++ keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
@@ -18,7 +18,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * | Tab    |   Q  |   W  |   E  |   R  |   T  | PLVR |           |      |   Y  |   U  |   I  |   O  |   P  |        |
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | L1*   |   A  |   S  |   D  |   F  |   G   |------|           |------|   H  |   J  |   K  |   L  | ;  : |        |
- * |--------+------+------+------+------+------|      |           | L2   |------+------+------+------+------+--------|
+ * |--------+------+------+------+------+------|      |           | ARRW |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  | /  ? | RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Ctrl |      |  Gui  | Alt  | L1   |                                       |  L1  | Alt  | Gui  |      | Ctrl |
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(PLVR),
     KC_FN4,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_LSFT,   KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
-    KC_LCTL,   KC_NO,   KC_LGUI, KC_LALT, MO(1),
+    KC_LCTL,   KC_NO,   KC_LGUI, KC_LALT, MO(SYMB),
 
     /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    KC_HOME, KC_END,
     /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_PGUP,
@@ -44,11 +44,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     //
 
-    /*-*/      KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_NO,
-    /*-*/      KC_NO,   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_NO,
-    /*-*/      /*-*/    KC_H,    KC_J,    KC_K,    KC_L,    KC_COLN,  KC_NO,
-    /*-*/      TG(2),   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-    /*-*/      /*-*/    /*-*/    MO(1),   KC_RALT, KC_RGUI, KC_NO,    KC_RCTL,
+    /*-*/      KC_NO,   KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_NO,
+    /*-*/      KC_NO,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,     KC_NO,
+    /*-*/      /*-*/    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_NO,
+    /*-*/      TG(ARRW),KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+    /*-*/      /*-*/    /*-*/    MO(SYMB), KC_RALT, KC_RGUI, KC_NO,    KC_RCTL,
 
     KC_LEFT,   KC_RGHT,
     KC_UP,
@@ -76,7 +76,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 |      |      |      |       |      |      |      |
  *                                 `--------------------'       `--------------------'
  */
-
   [SYMB] = KEYMAP(
     KC_TRNS,   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS,   KC_GRV,  KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,
@@ -101,7 +100,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TRNS,   KC_ESC, KC_TAB
   ),
 
-
+/* Keymap 2: Plover Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |  1   |  2   |  3   |  4   |  5   |      |           |      |  6   |  7   |  8   |  9   |  0   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |  q   |  w   |  e   |  r   |  t   |------|           |------|  y   |  u   |  i   |  o   |  p   |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |  a   |  s   |  d   |  f   |  g   |      |           |      |  h   |  j   |  k   |  l   |  :   |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `--------------------'------------'                                        `---------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |  c   |  v   |------|       |------|  n   |   m  |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
   [PLVR] = KEYMAP(
     KC_NO,     KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_TRNS,
     KC_NO,     KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_TRNS,
@@ -117,8 +136,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     /*-*/      KC_TRNS, KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     /*-*/      KC_NO,   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,     KC_NO,
-    /*-*/      /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,
-    /*-*/      KC_TRNS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
+    /*-*/      /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_NO,
+    /*-*/      KC_TRNS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_NO,
     /*-*/      /*-*/    /*-*/    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
     KC_TRNS,   KC_TRNS,
     KC_TRNS,
@@ -126,10 +145,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 
 
+/* Keymap 4: Arrow Layer
+ *
+ * ,--------------------------------------------------.           ,--------------------------------------------------.
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |  ↑   |      |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |------|           |------|      |  ←   |  ↓   |  →   |      |        |
+ * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
+ * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
+ *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   `--------------------'------------'                                        `---------------------------------'
+ *                                        ,-------------.       ,-------------.
+ *                                        |      |      |       |      |      |
+ *                                 ,------|------|------|       |------+------+------.
+ *                                 |      |      |      |       |      |      |      |
+ *                                 |      |      |------|       |------|      |      |
+ *                                 |      |      |      |       |      |      |      |
+ *                                 `--------------------'       `--------------------'
+ */
   [ARRW] = KEYMAP(
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_BTN1, KC_MS_U, KC_BTN2, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_MS_L, KC_MS_D, KC_MS_R, KC_TRNS,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
