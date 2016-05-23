cd5cc4fe9be1235c445bec8ec91c31debdbff2e3
Chris Chambers <magma.chambers@gmail.com>
2016-05-23 12:02:30 +0100

    Change symbol momentary layer shift to oneshot

--- keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
+++ keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(PLVR),
     KC_FN4,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
     KC_FN5,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
-    KC_LCTL,   KC_NO,   KC_LGUI, KC_LALT, MO(SYMB),
+    KC_LCTL,   KC_NO,   KC_LGUI, KC_LALT, KC_FN4,
 
     /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    KC_HOME, KC_END,
     /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_PGUP,
@@ -48,7 +48,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*-*/      KC_NO,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,     KC_NO,
     /*-*/      /*-*/    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,  KC_FN4,
     /*-*/      TG(ARRW),KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_FN6,
-    /*-*/      /*-*/    /*-*/    MO(SYMB), KC_RALT, KC_RGUI, KC_NO,    KC_RCTL,
+    /*-*/      /*-*/    /*-*/    KC_FN4,   KC_RALT, KC_RGUI, KC_NO,    KC_RCTL,
 
     KC_LEFT,   KC_RGHT,
     KC_UP,
