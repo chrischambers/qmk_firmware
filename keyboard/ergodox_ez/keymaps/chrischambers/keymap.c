5bb2b7cfaf31817a0772a0d2ae0ab0e1f18db750
Chris Chambers <magma.chambers@gmail.com>
2016-05-23 13:22:42 +0100

    Tidy up arrangement

--- keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
+++ keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
@@ -32,27 +32,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
   [BASE] = KEYMAP(
-    KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_NO,
-    KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    TG(PLVR),
-    OSL(SYMB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,
-    KC_FN1,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,
-    KC_LCTL,   KC_NO,   KC_LGUI, KC_LALT, OSL(SYMB),
+    KC_ESC,    KC_1,     KC_2,    KC_3,    KC_4,      KC_5,    KC_NO,
+    KC_TAB,    KC_Q,     KC_W,    KC_E,    KC_R,      KC_T,    TG(PLVR),
+    OSL(SYMB), KC_A,     KC_S,    KC_D,    KC_F,      KC_G,
+    KC_FN1,    KC_Z,     KC_X,    KC_C,    KC_V,      KC_B,    KC_NO,
+    KC_LCTL,   KC_NO,    KC_LGUI, KC_LALT, OSL(SYMB),
 
-    /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    KC_HOME, KC_END,
-    /*-*/      /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_PGUP,
-    /*-*/      /*-*/    /*-*/    /*-*/    KC_BSPC, KC_DELT, KC_PGDN,
+    /*-*/      /*-*/     /*-*/    /*-*/    /*-*/      KC_HOME, KC_END,
+    /*-*/      /*-*/     /*-*/    /*-*/    /*-*/      /*-*/    KC_PGUP,
+    /*-*/      /*-*/     /*-*/    /*-*/    KC_BSPC,   KC_DELT, KC_PGDN,
 
     //
 
-    /*-*/      KC_NO,   KC_6,    KC_7,     KC_8,    KC_9,    KC_0,     KC_NO,
-    /*-*/      KC_NO,   KC_Y,    KC_U,     KC_I,    KC_O,    KC_P,     KC_NO,
-    /*-*/      /*-*/    KC_H,    KC_J,     KC_K,    KC_L,    KC_SCLN,  OSL(SYMB),
-    /*-*/      TG(ARRW),KC_N,    KC_M,     KC_COMM, KC_DOT,  KC_SLSH,  KC_FN2,
-    /*-*/      /*-*/    /*-*/    OSL(SYMB),KC_RALT, KC_RGUI, KC_NO, KC_RCTL,
+    /*-*/      KC_NO,    KC_6,    KC_7,      KC_8,    KC_9,    KC_0,    KC_NO,
+    /*-*/      KC_NO,    KC_Y,    KC_U,      KC_I,    KC_O,    KC_P,    KC_NO,
+    /*-*/      /*-*/     KC_H,    KC_J,      KC_K,    KC_L,    KC_SCLN, OSL(SYMB),
+    /*-*/      TG(ARRW), KC_N,    KC_M,      KC_COMM, KC_DOT,  KC_SLSH, KC_FN2,
+    /*-*/      /*-*/     /*-*/    OSL(SYMB), KC_RALT, KC_RGUI, KC_NO,   KC_RCTL,
 
     KC_LEFT,   KC_RGHT,
     KC_UP,
-    KC_DOWN,   KC_ENT,  KC_SPC
+    KC_DOWN,   KC_ENT,   KC_SPC
   ),
 
 /* Keymap 1: Symbol layer
