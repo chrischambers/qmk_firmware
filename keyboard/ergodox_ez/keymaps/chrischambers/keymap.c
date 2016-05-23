4f66d235c391ef7d76bddb91c12c2c3d48fb6b90
Chris Chambers <magma.chambers@gmail.com>
2016-05-23 13:25:04 +0100

    Whitespace adjustment

--- keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
+++ keyboard/ergodox_ez/keymaps/chrischambers/keymap.c
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
   [SYMB] = KEYMAP(
-    KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,  KC_F11,
+    KC_TRNS,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F11,
     KC_TRNS,   KC_GRV,  KC_TRNS, KC_LCBR, KC_RCBR, KC_TRNS, KC_TRNS,
     KC_TRNS,   KC_AT,   KC_AMPR, KC_LPRN, KC_RPRN, KC_TRNS,
     KC_TRNS,   KC_BSLS, KC_PIPE, KC_LBRC, KC_RBRC, KC_TRNS, KC_TRNS,
@@ -97,7 +97,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     KC_TRNS,   KC_TRNS,
     KC_TRNS,
-    KC_TRNS,   KC_ESC, KC_TAB
+    KC_TRNS,   KC_ESC,  KC_TAB
   ),
 
 /* Keymap 2: Plover Layer
@@ -139,6 +139,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*-*/      /*-*/    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,     KC_LBRC,
     /*-*/      KC_TRNS, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,  KC_QUOT,
     /*-*/      /*-*/    /*-*/    KC_NO,   KC_NO,   KC_NO,   KC_NO,    KC_NO,
+
     KC_TRNS,   KC_TRNS,
     KC_TRNS,
     KC_NO,     KC_N,    KC_M
@@ -167,27 +168,27 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *                                 `--------------------'       `--------------------'
  */
   [ARRW] = KEYMAP(
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-    /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS,
-    /*-*/          /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS,
-    /*-*/          /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/     /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS,
+    /*-*/     /*-*/    /*-*/    /*-*/    /*-*/    /*-*/    KC_TRNS,
+    /*-*/     /*-*/    /*-*/    /*-*/    KC_TRNS, KC_TRNS, KC_TRNS,
 
     //
 
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS,       KC_TRNS,
-    /*-*/          /*-*/    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS,       KC_TRNS,
-    /*-*/          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,       KC_TRNS,
-    /*-*/          /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/     KC_TRNS, KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/     /*-*/    KC_TRNS, KC_LEFT, KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,
+    /*-*/     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+    /*-*/     /*-*/    KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
 
-    KC_TRNS,       KC_TRNS,
+    KC_TRNS,  KC_TRNS,
     KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS
+    KC_TRNS,  KC_TRNS, KC_TRNS
   ),
 };
 
